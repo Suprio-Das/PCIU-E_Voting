384fa492af7b93984e8df93cbe89efdc426dbcc3
Suprio-Das <priyamd507@gmail.com>
2025-09-03 13:54:38 +0600

    Backend - PORT defined

--- Backend/index.js
+++ Backend/index.js
@@ -3,9 +3,13 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config();
 
+// Define PORT
+const PORT = process.env.PORT || 3000;
+
 // Create Express APP
 const app = express();
 
 // Middlewares
 app.use(express.json())
 app.use(cors())
+
