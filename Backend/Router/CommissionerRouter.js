dd15ff208aeb80b5852581268045e4b3f88373b6
Suprio-Das <priyamd507@gmail.com>
2025-09-03 17:13:19 +0600

    Backend - AuthRoutes created

--- Backend/Router/CommissionerRouter.js
+++ Backend/Router/CommissionerRouter.js
@@ -2,4 +2,8 @@ import express from 'express'
 
 const CommissionerRoutes = express.Router();
 
+CommissionerRoutes.post('/createvote', (req, res) => {
+    console.log("Create vote route.")
+})
+
 export default CommissionerRoutes;
