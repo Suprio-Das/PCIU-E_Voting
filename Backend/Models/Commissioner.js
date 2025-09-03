dfbc714c3f5eca537d2c6d55968fa1600b69149f
Suprio-Das <priyamd507@gmail.com>
2025-09-03 17:25:57 +0600

    Backend - Commissioner model created

---
+++ Backend/Models/Commissioner.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const CommissionerSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['commissioner', 'student'],
+        default: 'student'
+    }
+})
