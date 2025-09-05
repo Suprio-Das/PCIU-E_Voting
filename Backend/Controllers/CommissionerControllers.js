d852ce408f092c78b111d63bd19d2a83b02a76e5
Suprio-Das <priyamd507@gmail.com>
2025-09-05 23:23:14 +0600

    Backend - Election stop controller designed

--- Backend/Controllers/CommissionerControllers.js
+++ Backend/Controllers/CommissionerControllers.js
@@ -24,3 +24,27 @@ export const StartElection = async (req, res) => {
         return res.send(error)
     }
 }
+
+export const StopElection = async (req, res) => {
+    try {
+        const elections = await VotingStatusModel.find({})
+        const election = elections[0];
+        if (election.active === false) {
+            return res.status(405).json({ success: false, message: "Election is not running. Start election first." })
+        }
+
+        const query = { _id: new ObjectId(election._id) }
+
+        const updateElection = {
+            $set: {
+                active: false
+            }
+        }
+
+        const newUpdatedElection = await VotingStatusModel.updateOne(query, updateElection)
+
+        res.status(200).json({ success: true, message: "Election has ended." }, newUpdatedElection)
+    } catch (error) {
+        return res.send(error)
+    }
+}
--- Backend/Router/CommissionerRouter.js
+++ Backend/Router/CommissionerRouter.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { isCommissioner } from '../Middlewares/verifyToken.js';
-import { StartElection } from '../Controllers/CommissionerControllers.js';
+import { StartElection, StopElection } from '../Controllers/CommissionerControllers.js';
 
 const CommissionerRoutes = express.Router();
 
 CommissionerRoutes.post('/createvote', isCommissioner, StartElection);
+CommissionerRoutes.post('/stopvote', isCommissioner, StopElection);
 
 export default CommissionerRoutes;
