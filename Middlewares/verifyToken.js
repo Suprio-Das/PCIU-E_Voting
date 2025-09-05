b57d17c977a59441968f4e1bb21a728a6c8c6600
Suprio-Das <priyamd507@gmail.com>
2025-09-05 20:06:49 +0600

    Backend - isCommissioner middleware token fetched from cookie

--- Middlewares/verifyToken.js
+++ Middlewares/verifyToken.js
@@ -1,6 +1,9 @@
 export const isCommissioner = async (req, res, next) => {
     try {
-        console.log("verify token middleware")
+        const token = await req.cookies.token;
+        if (!token) {
+            return res.status(401).json({ success: false, message: 'Unauthorized user.' })
+        }
     } catch (error) {
         return res.send(error);
     }
