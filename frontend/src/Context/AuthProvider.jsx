354b66b6c21fb0cbfeddb8df526571c4074cef69
Suprio-Das <priyamd507@gmail.com>
2025-09-19 22:04:44 +0600

    frontend - user and loading state created

--- frontend/src/Context/AuthProvider.jsx
+++ frontend/src/Context/AuthProvider.jsx
@@ -1,8 +1,10 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     return (
         <AuthContext.Provider>
             {children}
