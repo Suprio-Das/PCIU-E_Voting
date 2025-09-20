70fefa89b51eaa8e7437e0418d1a244026ac786d
Suprio-Das <priyamd507@gmail.com>
2025-09-20 22:37:18 +0600

    frontend - student route created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import CommissionerLayout from './Layouts/CommissionerLayout'
+import StudentLayout from './Layouts/StudentLayout'
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
         <Routes>
           <Route path='/commissioner' element={<CommissionerLayout></CommissionerLayout>}>
             <Route index element={<h1>Admin Route.</h1>}></Route>
+          </Route>
+          <Route path='/student' element={<StudentLayout></StudentLayout>}>
+            <Route index element={<h1>Student Route.</h1>}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
