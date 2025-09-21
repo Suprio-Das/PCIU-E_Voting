915dfb79927b75a13cc0a2ff1287e65db4d1dacb
Suprio-Das <priyamd507@gmail.com>
2025-09-21 21:59:15 +0600

    frontend - MainLayout wrapped over the all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import CommissionerLayout from './Layouts/CommissionerLayout'
 import StudentLayout from './Layouts/StudentLayout'
+import MainLayout from './Layouts/MainLayout'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/commissioner' element={<CommissionerLayout></CommissionerLayout>}>
-            <Route index element={<h1>Admin Route.</h1>}></Route>
-          </Route>
-          <Route path='/student' element={<StudentLayout></StudentLayout>}>
-            <Route index element={<h1>Student Route.</h1>}></Route>
+          <Route path='/' element={<MainLayout></MainLayout>}>
+            <Route path='/commissioner' element={<CommissionerLayout></CommissionerLayout>}>
+              <Route index element={<h1>Admin Route.</h1>}></Route>
+            </Route>
+            <Route path='/student' element={<StudentLayout></StudentLayout>}>
+              <Route index element={<h1>Student Route.</h1>}></Route>
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -3,7 +3,9 @@ import { Outlet } from 'react-router';
 const MainLayout = () => {
     return (
         <div>
+            <h1>Navbar</h1>
             <Outlet></Outlet>
+            <h1>Footer</h1>
         </div>
     );
 };
