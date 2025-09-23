932c74e9cacea54ec06cad2c0f8526503b16e30f
Suprio-Das <priyamd507@gmail.com>
2025-09-23 21:21:51 +0600

    frontend - password field added

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -4,7 +4,11 @@ const Login = () => {
             <form className="w-[40%] border-1 p-5">
                 <h1 className="lg:text-2xl font-bold text-blue-600 text-center my-3">Login as Commissioner</h1>
                 <fieldset className="fieldset w-full">
-                    <legend className="fieldset-legend">Email:</legend>
+                    <legend className="fieldset-legend text-lg">Email:</legend>
+                    <input type="text" className="input w-full" placeholder="Type here" />
+                </fieldset>
+                <fieldset className="fieldset w-full">
+                    <legend className="fieldset-legend text-lg">Password:</legend>
                     <input type="text" className="input w-full" placeholder="Type here" />
                 </fieldset>
             </form>
