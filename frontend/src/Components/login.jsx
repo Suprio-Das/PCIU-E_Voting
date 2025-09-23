1bb89e37a693c49ded8df8c562fadb52002516c6
Suprio-Das <priyamd507@gmail.com>
2025-09-23 21:18:51 +0600

    frontend - login page title and email field added

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -1,7 +1,13 @@
 const Login = () => {
     return (
         <div className="min-h-[calc(100vh-120px)] flex justify-center items-center">
-            <h1>This is login page.</h1>
+            <form className="w-[40%] border-1 p-5">
+                <h1 className="lg:text-2xl font-bold text-blue-600 text-center my-3">Login as Commissioner</h1>
+                <fieldset className="fieldset w-full">
+                    <legend className="fieldset-legend">Email:</legend>
+                    <input type="text" className="input w-full" placeholder="Type here" />
+                </fieldset>
+            </form>
         </div>
     );
 };
