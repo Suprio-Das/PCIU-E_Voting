b1343302a252d7a864ed51164a36d8bda65e80d7
Suprio-Das <priyamd507@gmail.com>
2025-09-23 21:36:46 +0600

    frontend - handle login function created

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -1,14 +1,18 @@
 const Login = () => {
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log("Handle Login")
+    }
     return (
         <div className="min-h-[calc(100vh-120px)] flex justify-center items-center">
-            <form className="w-[40%] shadow-lg border-1 border-[#2a3793] rounded-xl p-5">
+            <form className="w-[40%] shadow-lg border-1 border-[#2a3793] rounded-xl p-5" onSubmit={handleLogin}>
                 <h1 className="lg:text-2xl font-bold primary-color text-center my-3">Login as Commissioner</h1>
                 <fieldset className="fieldset w-full">
-                    <legend className="fieldset-legend text-lg">Email:</legend>
+                    <legend className="fieldset-legend text-lg">Email</legend>
                     <input type="text" className="input w-full outline-0 border-1 border-[#2a3793] focus-visible:outline-0 focus-visible:border-2" placeholder="Type here" />
                 </fieldset>
                 <fieldset className="fieldset w-full">
-                    <legend className="fieldset-legend text-lg">Password:</legend>
+                    <legend className="fieldset-legend text-lg">Password</legend>
                     <input type="text" className="input w-full outline-0 border-1 border-[#2a3793] focus-visible:outline-0 focus-visible:border-2" placeholder="Type here" />
                 </fieldset>
                 <button className="btn bg-[#2a3793] text-white w-full my-3">Login</button>
