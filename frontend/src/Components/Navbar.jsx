2d656852778538f15939af84a2fb354e68a877ae
Suprio-Das <priyamd507@gmail.com>
2025-09-23 21:29:22 +0600

    frontend - navbar updated with the primary color

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
                     <a className='flex items-center gap-2'>
                         <img src={Logo} alt="" className='w-14 h-18' />
                         <div>
-                            <h1 className='font-bold text-blue-600 text-xl -mb-1'>PCIU E-Voting</h1>
+                            <h1 className='font-bold primary-color text-xl -mb-1'>PCIU E-Voting</h1>
                             <p className='text-gray-500 text-sm'>Powered By CSE.</p>
                         </div>
                     </a>
                 </div>
                 <div className="flex-none">
-                    <button className="btn bg-blue-600 mr-5 text-white">Login</button>
+                    <button className="btn bg-[#2a3793] mr-5 text-white">Login</button>
                 </div>
             </div>
         </div>
