809b87f7c51a3c9baeb6eeebfee17bad405c4454
Suprio-Das <priyamd507@gmail.com>
2025-09-23 21:13:38 +0600

    frontend - login page centered vertically and horizontally

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -1,6 +1,6 @@
 const Login = () => {
     return (
-        <div>
+        <div className="min-h-[calc(100vh-120px)] flex justify-center items-center">
             <h1>This is login page.</h1>
         </div>
     );
