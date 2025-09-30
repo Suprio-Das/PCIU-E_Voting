90c26bdd6f9c656661260984e94f5876ef8d4f41
Suprio-Das <priyamd507@gmail.com>
2025-09-30 20:43:22 +0600

    frontend - AuthSlice implemented

--- frontend/src/Redux/AuthSlice.js
+++ frontend/src/Redux/AuthSlice.js
@@ -1,1 +1,20 @@
-console.log("AuthSlice")
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    loading: null,
+    error: null,
+    user: null
+}
+
+const AuthSlice = createSlice({
+    name: "AuthSlice",
+    initialState: initialState,
+    reducers: {
+        SetUser: (state, action) => {
+            state.user = action.payload;
+        }
+    }
+})
+
+export default AuthSlice.reducer;
+export const { SetUser } = AuthSlice.actions;
