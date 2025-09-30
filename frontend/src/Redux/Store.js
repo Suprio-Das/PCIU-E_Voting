b971820d645d26d387c6cf941b4b985b04e260f9
Suprio-Das <priyamd507@gmail.com>
2025-09-30 20:44:51 +0600

    frontend - Redux Store implemented

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,1 +1,22 @@
-console.log("Store")
+import { configureStore } from "@reduxjs/toolkit";
+import AuthSlice from './AuthSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
+
+// Persist Configuration
+const persistConfig = {
+    key: "root",
+    storage,
+    // whitelist: ['Auth']
+}
+
+// Persist Reducer
+const persistedReducer = persistReducer(persistConfig, AuthSlice);
+
+export const store = configureStore({
+    reducer: {
+        Auth: persistedReducer
+    }
+})
+
+export const persistor = persistStore(store);
