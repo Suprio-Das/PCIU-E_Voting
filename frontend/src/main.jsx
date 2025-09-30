66bd94fd4aaaae1e486ccb4c73f4d95716f8c190
Suprio-Das <priyamd507@gmail.com>
2025-09-30 20:47:39 +0600

    frontend - main wrapped with redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AuthProvider from './Context/AuthProvider.jsx'
+import { Provider } from 'react-redux'
+import { persistor, store } from './Redux/Store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
