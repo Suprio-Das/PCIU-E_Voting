9b302c5c93afb5ebda8243abcdd8ef8f2799b558
Suprio-Das <priyamd507@gmail.com>
2025-10-04 22:34:34 +0600

    frontend - footer designed

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,7 +1,12 @@
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+                <aside>
+                    <p>Desgined and Developed by: <a href="https://github.com/Suprio-Das" target="_blank" className="text-blue-600 font-semibold">Suprio Das</a></p>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by <span className="text-blue-600 font-semibold">Dept. of CSE, Port City International University</span></p>
+                </aside>
+            </footer>
         </div>
     );
 };
--- frontend/src/Layouts/MainLayout.jsx
+++ frontend/src/Layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router';
 import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            <h1>Footer</h1>
+            <Footer></Footer>
         </div>
     );
 };
