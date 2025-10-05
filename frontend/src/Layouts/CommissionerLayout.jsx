1123eafe366bc3fd7ee4b191eff1258504aff5b4
Suprio-Das <priyamd507@gmail.com>
2025-10-05 22:04:21 +0600

    frontend - Commissioner layout private

--- frontend/src/Layouts/CommissionerLayout.jsx
+++ frontend/src/Layouts/CommissionerLayout.jsx
@@ -1,6 +1,15 @@
+import { useSelector } from "react-redux";
 import Commissioner from "../Components/Commissioner";
+import { Navigate } from "react-router";
 
 const CommissionerLayout = () => {
+    const user = useSelector((state) => state.Auth.user);
+    if (!user) {
+        return <Navigate to='/'></Navigate>;
+    }
+    if (user.role !== 'commissioner') {
+        return <Navigate to='/'></Navigate>;
+    }
     return (
         <div>
             <Commissioner></Commissioner>
