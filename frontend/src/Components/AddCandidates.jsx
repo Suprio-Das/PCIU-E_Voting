529618806936f8ba7a23032d99e5c864575b4c87
Suprio-Das <priyamd507@gmail.com>
2025-10-07 00:41:40 +0600

    frontend - Candidate position added

--- frontend/src/Components/AddCandidates.jsx
+++ frontend/src/Components/AddCandidates.jsx
@@ -14,6 +14,16 @@ const AddCandidates = () => {
                         <legend className="fieldset-legend">Enter Candidate Student ID: </legend>
                         <input type="text" className="input w-full outline-0 border-1 border-[#2a3793] focus-visible:outline-0 focus-visible:border-2" placeholder="Type here" />
                     </fieldset>
+                    {/* Candidate Position */}
+                    <fieldset className="fieldset">
+                        <legend className="fieldset-legend">Select Candidate Position</legend>
+                        <select defaultValue="Select a position" className="select w-full border-1 border-[#2a3793] rounded-md focus:outline-none focus:border-blue-500">
+                            <option disabled={true}>Select a position</option>
+                            <option>President</option>
+                            <option>General Secretary</option>
+                            <option>IT Secretary</option>
+                        </select>
+                    </fieldset>
                 </form>
             </div>
         </div>
