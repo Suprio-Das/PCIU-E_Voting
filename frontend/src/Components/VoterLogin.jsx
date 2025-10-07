74d21fe04760576f514c0bbfd7490c265589d954
Suprio-Das <priyamd507@gmail.com>
2025-10-08 00:36:19 +0600

    frontend - voter login page created and integrated with backend

--- frontend/src/Components/VoterLogin.jsx
+++ frontend/src/Components/VoterLogin.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+import api from "../Services/api";
+import { useNavigate } from "react-router";
+
 const VoterLogin = () => {
+    const [studentId, setStudentId] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const res = await api.post("api/vote/verifyvoter", { studentId });
+            if (res.data.success) {
+                navigate("/student/vote", { state: { studentId } });
+            }
+        } catch (error) {
+            if (error.response) {
+                setError(error.response.data.message);
+            } else {
+                setError("Something went wrong. Please try again.");
+            }
+        }
+    };
+
     return (
-        <div>
-            <h1>Voter login page.</h1>
+        <div className="flex items-center justify-center min-h-[calc(100vh-170px)] bg-base-200">
+            <div className="bg-base-100 p-8 rounded-xl shadow-md w-[400px]">
+                <h1 className="text-3xl font-semibold text-center mb-4 text-[#2a3793]">
+                    Voter Login
+                </h1>
+                <form onSubmit={handleLogin} className="flex flex-col gap-4">
+                    <input
+                        type="text"
+                        placeholder="Enter your Student ID"
+                        className="input input-bordered border-[#2a3793] focus:outline-none"
+                        value={studentId}
+                        onChange={(e) => setStudentId(e.target.value)}
+                        required
+                    />
+                    {error && <p className="text-red-500 text-center">{error}</p>}
+                    <button className="btn bg-[#2a3793] text-white w-full">
+                        Proceed to Vote
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
