2a9cc0f81cde847532dac1cc61a943826898fa3b
Suprio-Das <priyamd507@gmail.com>
2025-10-07 10:16:13 +0600

    backend - GetVoters route created

--- Backend/Router/VotingRouter.js
+++ Backend/Router/VotingRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { GetCandidateWithPosition, SubmitVote, VerifyVoter } from '../Controllers/VotingController.js';
+import { GetCandidateWithPosition, GetVoters, SubmitVote, VerifyVoter } from '../Controllers/VotingController.js';
 import { isVoter } from '../Middlewares/verifyToken.js';
 // import { isVoted } from '../Middlewares/verifyToken.js';
 
 const VotingRoutes = express.Router();
 
 VotingRoutes.get('/getcandidatewithposition', GetCandidateWithPosition);
+VotingRoutes.get('/getvoters', GetVoters);
 VotingRoutes.post('/verifyvoter', VerifyVoter);
 VotingRoutes.post('/submitvote', isVoter, SubmitVote);
 
