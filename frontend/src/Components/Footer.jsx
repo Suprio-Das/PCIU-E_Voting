913d77f61e122f8c55cbf158e9b2868c19e81161
Suprio-Das <priyamd507@gmail.com>
2025-10-11 01:05:46 +0600

    frontend - footer updated

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div>
             <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
                 <aside>
-                    <p>Desgined and Developed by: <a href="https://github.com/Suprio-Das" target="_blank" className="text-blue-600 font-semibold">Suprio Das, CSE 28A Day</a></p>
+                    <p>Desgined and Developed by: <a href="https://github.com/Suprio-Das" target="_blank" className="text-blue-600 font-semibold">Suprio Das, CSE-28-D</a></p>
                     <p>Copyright © {new Date().getFullYear()} - All right reserved by <span className="text-blue-600 font-semibold">Dept. of CSE, Port City International University</span></p>
                 </aside>
             </footer>
