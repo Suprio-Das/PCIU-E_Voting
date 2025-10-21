d798c01740d7882274ba84e141b543160235e4a8
Suprio-Das <priyamd507@gmail.com>
2025-10-22 01:26:27 +0600

    deploy setting

--- Backend/Middlewares/verifyToken.js
+++ Backend/Middlewares/verifyToken.js
@@ -5,6 +5,7 @@ import StudentModel from '../Models/Student.js';
 export const isCommissioner = async (req, res, next) => {
     try {
         const token = await req.cookies.token;
+        console.log(token)
         if (!token) {
             return res.status(401).json({ success: false, message: 'Unauthorized user.' })
         }
