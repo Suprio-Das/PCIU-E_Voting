775d9c04b8ba8af7b23feebd647ffefbe9d0eb8a
Suprio-Das <priyamd507@gmail.com>
2025-10-25 13:15:09 +0600

    frontend - logo updated

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div className="navbar bg-base-100 shadow-sm">
                 <div className="flex-1 ms-5">
                     <a className='flex items-center gap-2'>
-                        <img src={Logo} alt="" className='w-14 h-18' />
+                        <img src={Logo} alt="" className='w-18 h-18' />
                         <div>
                             <h1 className='font-bold primary-color text-xl -mb-1'>PCIU E-Voting</h1>
                             <p className='text-gray-500 text-sm'>Powered By CSE.</p>
